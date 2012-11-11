909c8ef4898370e626466be9e8e738feb2b59613
Abishek Bhat <bhat.abishek@gmail.com>
2012-11-11 15:53:05 +0530

    Added better santization for the string. Prunigng common domain indetifiers that is

--- markov.rb
+++ markov.rb
@@ -1,3 +1,4 @@
+#usage ruby -rubygems markov.rb <category>
 require 'json'
 companies_json=IO.read "company_grouped.json"
 companies=JSON.parse(companies_json)
@@ -5,7 +6,7 @@ vertical=ARGV[0]
 chain={}
 seperator=' '
 companies[vertical].each do |company|
-	name=company["name"].downcase.split(//)
+	name=company.downcase.split(//)
 	name_pair=name.zip(name.drop(1))
 	name_pair.delete_at(-1)
 	name_pair.each{|key,value| chain[key]||=[]<<value}
@@ -23,7 +24,7 @@ def generate(seperator,chain)
 end
 new_name=''
 until new_name.length>5 or new_name.length<10
-	new_name+=generate(seperator,chain)
+	new_name=generate(seperator,chain)
 end 
 puts new_name
 
--- parse.rb
+++ parse.rb
@@ -18,8 +18,17 @@ companies_group=companies.group_by do |company|
 	when /enterprise/
 		"enterprise"
 	end
+end
+def sanitize_name(name)
+	name=name.split " "
+	name=name.first.gsub(%r(\..*),"")
+	return name
+end 
+%w[web ecommerce software education mobile enterprise hardware].each do |category|
+  companies_group[category].map!{|company| sanitize_name(company["name"])}
 end 
 File.open("company_grouped.json",'w'){|f| f.write(companies_group.to_json)}
+
 
   
 
