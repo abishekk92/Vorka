e3ffc9202b07e9c75255f9cd704471a56293604f
abishekk92 <bhat.abishek@gmail.com>
2014-04-13 13:34:13 +0530

    Monte Carlo Method to maximize word probability
    The words or the startup names were created as a result of a random walk
    on the generated Markov Chain. As the name suggests random walk is
    completely random and the results can be random too.
    
    We assume every generation of the word as a simulation and evaluate the
    fitness of the outcome.
    
    ```ruby
        if score_val > 0.5
            puts word
            so_far += 1
        end
    ```
    
    Scoring of a geernated word is the ratio of the number letters with
    probability lying between the second and the third quartiles of the
    probabilities to the number of letters in the word.
    
    Evaluating it based on the probability distribution always tries to
    maximize the occurence of the most frequent letter and hence probably a
    name given the dataset

--- vorka.rb
+++ vorka.rb
@@ -5,18 +5,30 @@ def extract_letters(_string)
     _string.gsub(/[^A-Za-z]/, '').downcase.split //
 end
 
+def frequency(word)
+  Hash[extract_letters(word).group_by(&:chr).map { |k, v| [k, v.size] }]
+end
+
 def chain(companies, vertical)
     chain = {}
+    local_model = {}
     companies[vertical].each do |company|
         letters = extract_letters company
-        letters.each{|letter| chain[letter]||=[]}
+        letters.each do |letter|
+            chain[letter]||= []
+            local_model[letter]||= {}
+        end
     begin
-        letters[0..-2].zip(letters[1..-1]).each{|key,value| chain[key].push(value)}
+        letters[0..-2].zip(letters[1..-1]).each do |key,value|
+            chain[key].push(value)
+            local_model[key][value]||=0
+            local_model[key][value]+=1
+        end
     rescue
         puts letters
     end
 end
-    chain
+    return chain, local_model
 end
 
 def generate(length, chain)
@@ -25,9 +37,25 @@ def generate(length, chain)
 	while !items.empty? and result.length < length 
 		item = items.sample
 		result.push item
-        	items = chain[item]
+        items = chain[item]
 	end
 	result.join.capitalize 
+end
+
+def probability_model(frequencies)
+    total = frequencies.values.reduce(:+)
+    Hash[frequencies.map {|letter, count| [letter, count.to_f/total]}]
+end
+
+def quartiles_2_3(probabilities)
+    values = probabilities.values.sort
+    opt_group_size = values.size/3
+    (values[opt_group_size*2]...values[opt_group_size*3])
+end
+
+def score(word, probabilities, quartile_vals)
+    in_range = extract_letters(word).select{ |letter| quartile_vals.include? probabilities[letter] }.size
+    in_range.to_f/word.size
 end
 
 Encoding.default_external = Encoding::UTF_8
@@ -45,8 +73,18 @@ vertical = ARGV[0]
 length = ARGV[1].to_i
 num_name = ARGV[2].to_i
 
-companies.keys.each do |vertical|
-    chain = chain companies, vertical
-    puts vertical.upcase
-    num_name.times{|i| puts generate(length, chain)}
+chain, local_model = chain companies, vertical
+frequencies = frequency companies[vertical].join ""
+probabilities = probability_model frequencies
+quartile_vals = quartiles_2_3 probabilities
+
+so_far = 0
+
+until so_far == num_name
+    word = generate(length, chain)
+    score_val = score word, probabilities, quartile_vals
+    if score_val > 0.5
+        puts word
+        so_far += 1
+    end
 end
