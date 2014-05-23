28c833ad4a7ec76fea8f574a34c80c2583876448
abishekk92 <bhat.abishek@gmail.com>
2014-05-23 12:25:26 +0530

    Reactoring
        - Refactor genenrator and collector into recursive
        - Constructors for NameGenerator and FitnessValidator
        - FitnessScore class
        - Purge superflous use of module

--- vorka.rb
+++ vorka.rb
@@ -1,52 +1,57 @@
 require 'json'
 require_relative 'utils'
 
-module Vorka
-    def chain(companies, vertical)
-        chain = {}
-        companies[vertical].each do |company|
-            letters = extract_letters company
-            letters.each do |letter|
-                chain[letter]||= []
+class NameGenerator
+    
+    def initialize(companies, length)
+        @chain = chain companies
+        @length = length
+        @validator = FitnessValidator.new companies
+    end
+    
+    def generate(num_name, so_far=0)
+        unless so_far == num_name
+            word = generate_word
+
+            score = @validator.fitness word
+            if score.is_fit?
+                puts word
+                generate(num_name, so_far+1)
+            else
+                generate(num_name, so_far)
             end
-            unless letters.empty?
-                letters[0..-2].zip(letters[1..-1]).each do |key,value|
-                    chain[key].push(value)
-                end
         end
-    end
-       chain
-    end
-
-
-    class NameGenerator
-        
-        attr_accessor :chain
-
-        def generate(length)
-            items = self.chain.keys
-            result = []
-            while !items.empty? and result.length < length
-                item = items.sample
-                result.push item
-                items = self.chain[item]
-            end
+    end  
+    
+    def generate_word(items=@chain.keys, result=[])
+        if !items.empty? and result.length < @length
+            item = items.sample
+            generate_word(@chain[item], result.push(item))
+        else
             result.join.capitalize
         end
     end
-
-    class FitnessValidator
-        attr_accessor :probabilities, :quartiles_2_3
-        
-        def fitness(word)
-            in_range = extract_letters(word).select{ |letter| self.quartiles_2_3.include? self.probabilities[letter] }.size
-            in_range.to_f/word.size
-        end
-
-        def self.is_fit?(fitness_score)
-            fitness_score > 0.5
-        end
-
-    end
 end
 
+class FitnessValidator
+    
+    def initialize(companies)
+        @probabilities = probability_model frequency companies.join ""
+        @quartiles_2_3 = quartiles_2_3 @probabilities
+    end    
+    def fitness(word)
+        in_range = extract_letters(word).select{ |letter| @quartiles_2_3.include? @probabilities[letter] }.size
+        FitnessScore.new in_range.to_f/word.size
+    end
+
+end
+class FitnessScore
+    def initialize(score)
+        @score = score
+    end
+    
+    def is_fit?
+        @score > 0.5
+    end
+
+end
